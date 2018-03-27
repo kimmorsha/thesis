04b567767a3cfe076b15a39266800ecbb61179fc
ajrequina <ajrequina@up.edu.ph>
2018-03-27 16:52:41 +0800

    cleaned the tweets for all months

--- preprocess/remove_stopwords/clean_tweets_may.py
+++ preprocess/remove_stopwords/clean_tweets_may.py
@@ -92,17 +92,17 @@ def split_words(text):
 # csv_read_and_write("../../marawi_tweets_with_location/marawi_tweets_may/official/english_tweets/marawi_tweets_05_23.csv",
 #                    "../../marawi_tweets_with_location/marawi_tweets_may/official/cleaned_tweets/marawi_tweets_05_23.csv")
 
-# csv_read_and_write("../../marawi_tweets_with_location/marawi_tweets_may/official/english_tweets/marawi_tweets_05_24.csv",
-#                    "../../marawi_tweets_with_location/marawi_tweets_may/official/cleaned_tweets/marawi_tweets_05_24.csv")
+csv_read_and_write("../../marawi_tweets_with_location/marawi_tweets_may/official/english_tweets/marawi_tweets_05_24.csv",
+                   "../../marawi_tweets_with_location/marawi_tweets_may/official/cleaned_tweets/marawi_tweets_05_24.csv")
 
-# csv_read_and_write("../../marawi_tweets_with_location/marawi_tweets_may/official/english_tweets/marawi_tweets_05_25.csv",
-#                    "../../marawi_tweets_with_location/marawi_tweets_may/official/cleaned_tweets/marawi_tweets_05_25.csv")
+csv_read_and_write("../../marawi_tweets_with_location/marawi_tweets_may/official/english_tweets/marawi_tweets_05_25.csv",
+                   "../../marawi_tweets_with_location/marawi_tweets_may/official/cleaned_tweets/marawi_tweets_05_25.csv")
 
-# csv_read_and_write("../../marawi_tweets_with_location/marawi_tweets_may/official/english_tweets/marawi_tweets_05_26.csv",
-#                    "../../marawi_tweets_with_location/marawi_tweets_may/official/cleaned_tweets/marawi_tweets_05_26.csv")
+csv_read_and_write("../../marawi_tweets_with_location/marawi_tweets_may/official/english_tweets/marawi_tweets_05_26.csv",
+                   "../../marawi_tweets_with_location/marawi_tweets_may/official/cleaned_tweets/marawi_tweets_05_26.csv")
 
-# csv_read_and_write("../../marawi_tweets_with_location/marawi_tweets_may/official/english_tweets/marawi_tweets_05_27.csv",
-#                    "../../marawi_tweets_with_location/marawi_tweets_may/official/cleaned_tweets/marawi_tweets_05_27.csv")
+csv_read_and_write("../../marawi_tweets_with_location/marawi_tweets_may/official/english_tweets/marawi_tweets_05_27.csv",
+                   "../../marawi_tweets_with_location/marawi_tweets_may/official/cleaned_tweets/marawi_tweets_05_27.csv")
 
 # csv_read_and_write("../../marawi_tweets_with_location/marawi_tweets_may/official/english_tweets/marawi_tweets_05_28.csv",
 #                    "../../marawi_tweets_with_location/marawi_tweets_may/official/cleaned_tweets/marawi_tweets_05_28.csv")
